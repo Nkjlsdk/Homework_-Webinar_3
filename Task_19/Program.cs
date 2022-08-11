2583d3b24be93a82fa83ba77f1d0ed7eec4e2662
Your Name <e@w.com>
2022-08-12 01:21:28 +0300

    проверить является ли число полиндромом

---
+++ Task_19/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход пятизначное число, приверяет является ли оно палиндром
+int number = new Random().Next(10000, 100000);
+if (number/1000==(number%100)%10*10+((number%100))/10)
+{
+    Console.WriteLine($"{number} да");
+}
+else
+{
+    Console.WriteLine($"{number} нет");
+}
