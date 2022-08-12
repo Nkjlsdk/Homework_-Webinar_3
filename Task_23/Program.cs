7ba16f0b74d382ea97294002c6d5a5751827b167
Your Name <e@w.com>
2022-08-12 03:02:34 +0300

    Ввод числа N и вывод кубов чисел от 1 до N

---
+++ Task_23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ввод числа N и вывод кубов чисел от 1 до N
+int numberN = new Random().Next(1,10);
+int count = 0;
+int stepen = 3;
+Console.Write($"{numberN}- ");
+while (count<numberN)
+{
+        Console.Write($"{Math.Pow(count+1, stepen) } ");
+        count++;
+}
