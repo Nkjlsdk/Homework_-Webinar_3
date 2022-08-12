aa5b4c56c8eccfe62462f97908e9d034a291027e
Your Name <e@w.com>
2022-08-12 17:10:36 +0300

    поиск расстояния между двумя точками в 3D пространстве

---
+++ Task_21/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ввод координат двух точек и поиск расстояния между ними в 3D пространстве
+int pointBx = new Random().Next(1,100);
+int pointAx = new Random().Next(1,100);
+int pointBy = new Random().Next(1,100);
+int pointAy = new Random().Next(1,100);
+int pointBz = new Random().Next(1,100);
+int pointAz = new Random().Next(1,100);
+double result = Math.Sqrt(Convert.ToInt32(Math.Pow((pointBx-pointAx), 2))+Convert.ToInt32(Math.Pow((pointBy-pointAy), 2))+Convert.ToInt32(Math.Pow((pointBz-pointAz), 2)));
+Console.WriteLine($"A ({pointAx}, {pointAy}, {pointAz}); В ({pointBx}, {pointBy}, {pointBz})");
+Console.WriteLine($"AB= {result:0.00}");
