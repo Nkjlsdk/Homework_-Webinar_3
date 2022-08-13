c66fba7e78282a55440debff691d934b45c9cc0d
Your Name <e@w.com>
2022-08-13 14:51:59 +0300

    Ввод N и вывод чисел от 1 до Т в квадрате с помощью while

---
+++ Task_24/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ввести число N, вывести ряд квадратов чисел от 1 до N
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count  = 1;
+while (count<=N)
+{
+    Console.WriteLine(Math.Pow(count,2));
+    count++;
+}
