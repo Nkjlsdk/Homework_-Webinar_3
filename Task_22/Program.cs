c04a0fd0a8c6b9db72681176f97830da80f6c6c0
Your Name <e@w.com>
2022-08-13 14:14:29 +0300

    Ввод N, вывод квадратов чисел от 1 до N

---
+++ Task_22/Program.cs
@@ -0,0 +1,4 @@
+﻿// Ввести число N, вывести ряд квадратов чисел от 1 до N
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int count = 1; count<=N; count++) Console.WriteLine(Math.Pow(count,2));//for подобна while
