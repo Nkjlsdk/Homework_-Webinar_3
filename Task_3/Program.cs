fb9ed4e2453d6be0c91c0cd665d81bf4657164bd
Your Name <e@w.com>
2022-08-13 12:34:02 +0300

    по заданному номеру четверти определить координаты

---
+++ Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿/// по № четверть сист.координат  определить координаты положительные/отрицательные 
+Console.WriteLine("Введите номер четверти системы координат:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if (quarter == 1)  Console.WriteLine(" x > 0 && y > 0");
+if (quarter == 2)  Console.WriteLine(" x < 0 && y > 0");
+if (quarter == 3)  Console.WriteLine(" x < 0 && y < 0");
+if (quarter == 4)  Console.WriteLine(" x > 0 && y < 0");
