5eb30ac3de132c1a7b56df8004bf66c727e54398
Your Name <e@w.com>
2022-08-13 10:17:07 +0300

    проверка длины строки сколько символов в строке

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// проверка длины строки
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int size = number.Length; //ввод числовой переменной обозначающий длину
+Console.WriteLine(size);//вывод длины строки или цифры (сколько символов в введенном значинии)
