2c082924201d7c8c4c926ec01e049cff88b85737
Your Name <e@w.com>
2022-08-13 12:02:29 +0300

    определение по заданным х,у номера четверти координат

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// ввод координат точки по ним нужно определить четверть сист.координат  
+Console.WriteLine("Введите координату по оси оX:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату по оси оY:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)  Console.WriteLine("I четверть");
+if (x < 0 && y > 0)  Console.WriteLine("II четверть");
+if (x < 0 && y < 0)  Console.WriteLine("III четверть");
+if (x > 0 && y < 0)  Console.WriteLine("IV четверть");
